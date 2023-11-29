62a1e1642541a99abbbdaca7f090c42b66dbf43d
Ryan Costa <ryan.costa122@gmail.com>
2023-11-29 11:47:45 -0300

    fix: correção 7

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const withImages = require('next-images')
+const path = require('path')
 
 const nextConfig = withImages({
   experimental: {
@@ -10,6 +11,13 @@ const nextConfig = withImages({
       'consigaki.s3.sa-east-1.amazonaws.com',
       'consigaki.s3.amazonaws.com',
     ],
+  },
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': path.resolve(__dirname, './'),
+    }
+    return config
   },
 })
 
